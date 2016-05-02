5b962deaeeeb4094e8e7cfdcb1da5ce87c13920a
Ryan Batty <rgbatty@outlook.com>
2016-05-01 21:57:27 -0600

    Initial board implementation complete

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,61 @@
 class Board
-  def valid_coords?(coords)
-    true
+  attr_reader :size, :grid
+
+  def initialize
+    @size = 4
+    @grid = generate_grid(@size)
+  end
+
+  def generate_grid(size)
+    Array.new(size) do |x|
+      Array.new(size) do |y|
+        "~"
+      end
+    end
+  end
+
+  def set_position(position, value)
+    grid[position[0]][position[1]] = value
+  end
+
+  def occupied?(position)
+    return true if grid[position[0]][position[1]] != "~"
+    false
+  end
+
+  def out_of_bounds?(position)
+    if position[0] < 0 || position[0] >= size
+      true
+    elsif position[1] < 0 || position[1] >= size
+      true
+    else
+      false
+    end
+  end
+
+  def adjacent?(position_a, position_b)
+    if position_a[1] == position_b[1]+1
+      true
+    elsif position_a[0] == position_b[0]+1
+      true
+    elsif position_a[0] == position_b[0]-1
+      true
+    elsif position_a[1] == position_b[1]-1
+      true
+    else
+      false
+    end
+  end
+
+  def valid_positions?(positions)
+    positions.length.times do |i|
+      return false if out_of_bounds?(positions[i])
+      return false if occupied?(positions[i])
+    end
+
+    positions.length.times do |i|
+      return true if positions[i+1].nil?
+      return false if !adjacent?(positions[i], positions[i+1])
+    end
   end
 end
---
+++ test/board_test.rb
@@ -0,0 +1,115 @@
+require_relative 'test_helper'
+require_relative '../lib/board'
+
+class BoardTest < Minitest::Test
+  def setup
+    @board = Board.new
+  end
+
+  def test_it_exists
+    assert @board
+  end
+
+  def test_it_has_a_size
+    assert @board.size
+  end
+
+  def test_it_has_a_default_size_of_4
+    assert_equal 4, @board.size
+  end
+
+  def test_it_has_a_grid_of_positions
+    assert @board.grid
+    assert_kind_of Array, @board.grid
+  end
+
+  def test_it_can_generate_a_square_grid
+    @board.generate_grid(4)
+
+    assert_equal [["~","~","~","~"],
+                  ["~","~","~","~"],
+                  ["~","~","~","~"],
+                  ["~","~","~","~"]],
+                  @board.grid
+  end
+
+  def test_it_can_set_a_position_on_the_grid
+    @board.set_position([0,0], "s")
+
+    assert_equal "s", @board.grid[0][0]
+  end
+
+  def test_it_can_check_if_a_given_position_is_not_occupied
+    refute @board.occupied?([0,0])
+  end
+
+  def test_it_can_check_if_a_given_position_is_occupied
+    @board.set_position([0,0], "s")
+
+    assert @board.occupied?([0,0])
+  end
+
+  def test_it_can_check_if_a_position_is_out_of_bounds
+    result_1 = @board.out_of_bounds?([0,-1])
+    result_2 = @board.out_of_bounds?([0,4])
+    result_3 = @board.out_of_bounds?([-1,0])
+    result_4 = @board.out_of_bounds?([4,0])
+
+    assert result_1
+    assert result_2
+    assert result_3
+    assert result_4
+  end
+
+  def test_it_can_check_if_a_position_is_in_bounds
+    result = @board.out_of_bounds?([0,0])
+
+    refute result
+  end
+
+  def test_it_can_check_if_a_position_is_adjacent_to_another
+    result_1 = @board.adjacent?([1,1],[1,2])
+    result_2 = @board.adjacent?([1,1],[2,1])
+    result_3 = @board.adjacent?([1,1],[0,1])
+    result_4 = @board.adjacent?([1,1],[1,0])
+
+    assert result_1
+    assert result_2
+    assert result_3
+    assert result_4
+  end
+
+  def test_it_can_check_if_a_position_is_not_adjacent_to_another
+    result = @board.adjacent?([0,0],[0,2])
+
+    refute result
+  end
+
+  def test_it_can_check_if_a_series_of_two_positions_are_valid
+    result = @board.valid_positions?([[0,0],[0,1]])
+
+    assert result
+  end
+
+  def test_it_can_check_if_a_series_of_two_or_more_positions_are_valid
+    result = @board.valid_positions?([[0,0],[0,1],[0,2]])
+
+    assert result
+  end
+
+  def test_it_can_invalidate_a_series_of_positions_if_one_is_occupied
+    @board.set_position([0,0], "s")
+    result = @board.valid_positions?([[0,0],[0,1],[0,2]])
+
+    refute result
+  end
+
+  def test_it_can_invalidate_a_series_of_positions_that_are_not_adjacent
+    result_1 = @board.valid_positions?([[0,0],[0,2],[0,3]])
+    result_2 = @board.valid_positions?([[0,0],[0,1],[0,3]])
+
+    refute result_1
+    refute result_2
+  end
+
+end
